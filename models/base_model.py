ff5eab03a00194ef402a766bb0ac61db21badddf
RedarsHubi <RedarsHubi@github.com>
2023-12-07 07:40:28 -0800

    5

--- models/base_model.py
+++ models/base_model.py
@@ -12,19 +12,20 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
-        if kwargs is not None:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+        else:
             for key, value in kwargs.items():
                 if (key == "__class__"):
                     continue
                 if (key == "created_at" or key == "updated_at"):
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
-        else:
-           models.storage.new(self)
 
     def __str__(self):
         streeng = "[" + str(self.__class__.__name__) + "] ("
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,17 +11,19 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        key = obj.__class__.__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)."""
         serialized_objects = {}
+        print("save method is called")
         for key, obj in FileStorage.__objects.items():
             if isinstance(obj, BaseModel):
+                print("obj to be saved")
                 serialized_objects[key] = obj.to_dict()
         with open(FileStorage.__file_path, 'w') as f:
             json.dump(serialized_objects, f)
@@ -32,10 +34,11 @@ class FileStorage:
             with open(self.__file_path, 'r') as f:
                 data = json.load(f)
                 for key, obj_data in data.items():
-                     self.__objects[key] = obj_data
+                    class_name, obj_id = key.split('.')
+                    class_type = eval(class_name)
+                    obj_instance = class_type(**obj_data)
+                    self.__objects[key] = obj_instance
         except json.decoder.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
         except FileNotFoundError:
             pass
-
-
